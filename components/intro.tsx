388b4d328fabf6210c13c9e19192affc3177bcb0
RezaDarestani <rezafluttercare@gmail.com>
2025-04-02 22:21:12 -0700

    update the intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,11 +62,11 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hello, I'm Reza.</span> I'm a{" "}
+        <span className="font-bold">Hardware/Firmware developer</span> with{" "}
+        <span className="font-bold">10 years</span> of experience. I enjoy
+        building <span className="italic">developing boards & programming</span>. My focus is{" "}
+        <span className="underline">on wearable devices, C/C++, Python, RTOS. </span>.
       </motion.h1>
 
       <motion.div
